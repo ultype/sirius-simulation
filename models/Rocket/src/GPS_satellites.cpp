7c61ef33111417cb360abc1ab6af95499aa38edf
sonicyang <sonic.tw.tp@gmail.com>
2016-09-02 20:19:46 +0800

    [GPS_SAT] Use correct method to acquire simulation elapsed time

--- models/Rocket/src/GPS_satellites.cpp
+++ models/Rocket/src/GPS_satellites.cpp
@@ -25,13 +25,13 @@ void GPS_Satellites::initialize(){
 
 void GPS_Satellites::propagate_sv(){
     // unpacking the one-dimensional array of SVs into sv_data[24][2]
-    for (i = 0; i < 24; i++) {
+    for (int i = 0; i < 24; i++) {
         for (int k = 0; k < 2; k++) {
             sv_data[i][k] = *(sv_init_data + 2 * i + k);
         }
     }
     // propagating the argument of latitude in time
-    for (i = 0; i < 24; i++) {
-        sv_data[i][1] = sv_data[i][1] + (almanac_time + (double)(the_clock->clock_time()) / the_clock->clock_tics_per_sec) * velocity;
+    for (int i = 0; i < 24; i++) {
+        sv_data[i][1] = sv_data[i][1] + (almanac_time + get_rettime()) * angular_velocity;
     }
 }
