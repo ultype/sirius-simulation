bdd442691fe02821e117a1a90c21802ede968601
sonicyang <sonic.tw.tp@gmail.com>
2016-09-11 00:13:31 +0800

    [INS] Add computed position, velocity and angular velocity in model

--- models/Rocket/include/INS.hh
+++ models/Rocket/include/INS.hh
@@ -15,6 +15,10 @@ class INS {
 
     void default_data();
     void initialize();
+
+    double IPos[3];     /* *i (m)       Body in Inertial Coordinate */
+    double IVel[3];     /* *i (m)       Body in Inertial Coordinate */
+    double IW[3];       /* *i (m)       Body in Inertial Coordinate */
 };
 
 #endif  // __INS_HH__
