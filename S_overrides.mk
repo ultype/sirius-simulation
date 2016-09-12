0046a8b28f72cb801331c047a0be629c8da90023
sonicyang <sonic.tw.tp@gmail.com>
2016-09-13 02:45:42 +0800

    [BUILD] Enable debug

--- S_overrides.mk
+++ S_overrides.mk
@@ -1,3 +1,3 @@
 
-TRICK_CFLAGS += -I${TRICK_HOME}/trick_models -Imodels
-TRICK_CXXFLAGS += -I${TRICK_HOME}/trick_models -Imodels
+TRICK_CFLAGS += -I${TRICK_HOME}/trick_models -Imodels -g
+TRICK_CXXFLAGS += -I${TRICK_HOME}/trick_models -Imodels -g
