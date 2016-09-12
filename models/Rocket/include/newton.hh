bc63065ae4980fa7e58a1e062a1ee8a878f37925
sonicyang <sonic.tw.tp@gmail.com>
2016-09-13 00:35:16 +0800

    [NEWTON] Refine and Properly link with other models

--- models/Rocket/include/newton.hh
+++ models/Rocket/include/newton.hh
@@ -4,7 +4,7 @@
 PURPOSE:
       (Describe the newton Module Variables and Algorithm)
 LIBRARY DEPENDENCY:
-      ((../src/newton.cpp))
+      ((../src/Newton.cpp))
 PROGRAMMERS:
       (((Lai Jun Xu) () () () ))
 *******************************************************************************/
@@ -12,53 +12,64 @@ PROGRAMMERS:
 #include "global_constants.hh"
 #include "utility_header.hh"
 
+#include "Environment.hh"
+#include "Kinematics.hh"
+//#include "Propulsion.hh"
+#include "Force.hh"
+#include "Euler.hh"
+
+class Kinematics;
+
+class Propulsion{ public: double vmass;};
+
 class Newton {
     public:
     Newton() {}
 
-    Matrix VBEB(3,1);
-    Matrix TVD(3,3);
-    Matrix WBII(3,1);
-    Matrix VBED(3,1);
-    Matrix SBII(3,1);
-    Matrix VBII(3,1);
-    Matrix ABII(3,1);
-    Matrix WEII(3,3);
-    Matrix TDI(3,3);
-    Matrix TGI(3,3);
-    Matrix FSPB(3,1);
+    Kinematics *kinematics;
+    Environment *environment;
+    _Euler_ *euler;
+    Propulsion *propulsion;
+    Forces *forces;
 
     void default_newton();
-    void initialize_newton(kinematics* kine);
-    void newton(double int_step, Kinematics *kine, Environment *Env, Propulsion *Prop, forces *forc);
+    void initialize_newton(Kinematics *kine, _Euler_ *elr, Environment *Env, Propulsion *Prop, Forces *forc);
+    void newton(double int_step);
+
+    /* Input data */
 /***********************************************Variables description******************************************/
-    double alpha0x;     /* *io (d)      Initial angle-of-attack */
-    double beta0x;      /* *io (d)      Initial sideslip-angle */
+    double alpha0x;     /* *i (d)      Initial angle-of-attack */
+    double beta0x;      /* *i (d)      Initial sideslip-angle */
+
+    double dvbe;        /* *io (m/s)    Vehicle geographic speed */
     double lonx;        /* *io (d)      Vehicle longitude */
     double latx;        /* *io (d)      Vehicle latitude */
     double alt;         /* *io (m)      Vehicle altitude */
-    double tvd[9];      /* *io (--)     Transformation Matrix of geographic velocity wrt geodetic coord */
-    double tdi[9];      /* *io (--)     Transformation Matrix of geodetic wrt inertial  coordinates */
-    double dvbe;        /* *io (m/s)    Vehicle geographic speed */
-    double dvbi;        /* *io (m/s)    Vehicle inertia speee */
-    double weii[9];     /* *io (r/s)    Earth's angular velocity (skew-sym) */
+
     double psivdx;      /* *io (d)      Vehicle's heading angle */
     double thtvdx;      /* *io (d)      Vehicle's flight path angle */
+
+    double tvd[3][3];   /* *io (--)     Transformation Matrix of geographic velocity wrt geodetic coord */
+    double tdi[3][3];   /* *io (--)     Transformation Matrix of geodetic wrt inertial  coordinates */
+
+    double dvbi;        /* *io (m/s)    Vehicle inertia speee */
+    double weii[3][3];  /* *io (r/s)    Earth's angular velocity (skew-sym) */
     double dbi;         /* *io (m)      Vehicle distance from center of earth */
-    double tgi[9];      /* *io (--)     Transformation Matrix geocentric wrt inertia coord */
+    double tgi[3][3];   /* *io (--)     Transformation Matrix geocentric wrt inertia coord */
     double vbed[3];     /* *io (m/s)    Geographic velocity in geodetic coord */
     double altx;        /* *io (ft)     Vehicle altitude - ft */
     double IPos[3];     /* *io (m)      Vehicle position in inertia coord */
     double IVel[3];     /* *io (m/s)    Vehicle inertia velocity */
-    double IAccl[3];    /* *io (m/s2)  Vehicle inertia acceleration */
+    double IAccl[3];    /* *io (m/s2)   Vehicle inertia acceleration */
     double grndtrck;    /* *io (m)      Vehicle ground track on earth */
-    double fspb[3];     /* *io (m/s2)  Specific force in body coord */
-    double ayx;         /* *io (m/s2)  Achieved side acceleration */
-    double anx;         /* *io (m/s2)  Achieved normal acceleration */
+    double fspb[3];     /* *io (m/s2)   Specific force in body coord */
+    double ayx;         /* *io (m/s2)   Achieved side acceleration */
+    double anx;         /* *io (m/s2)   Achieved normal acceleration */
     double gndtrkmx;    /* *io (km)     Ground track - km */
     double gndtrnmx;    /* *io (nm)     Ground track - nm */
-    double aero_loss;   /* *io (m/s)    Speed loss caused by aerodynamic drag */
-    double gravity_loss;/* *io (m/s)    Speed loss caused by gravity */
+    double aero_loss;   /* *io (m/s)    Velocity loss caused by aerodynamic drag */
+    double gravity_loss;/* *io (m/s)    Velocity loss caused by gravity */
+    double gravity_loss2;/* *io (m/s)   Velocity loss caused by gravity */
 /*************************************************************************************************************/
 
 
--- models/Rocket/src/Newton.cpp
+++ models/Rocket/src/Newton.cpp
@@ -5,25 +5,41 @@
 #include "trick_utils/math/include/trick_math_proto.h"
 #include "sim_services/include/simtime.h"
 
+#include "utility_header.hh"
+
 void Newton::default_newton(){
 }
 
-void Newton::initialize_newton(Kinematics& kine){
-    Kinematics = &kine;
+void Newton::initialize_newton(Kinematics *kine, _Euler_ *elr, Environment *env, Propulsion *prop, Forces *forc){
+    kinematics = kine;
+    euler = elr;
+    environment = env;
+    propulsion = prop;
+    forces = forc;
 
-    double psibdx=Kinematics->psibdx;
-    double thtbdx=Kinematics->thtbdx;
-    double phibdx=Kinematics->phibdx;
+    Matrix WBII(3, 3);
+    double dbi(0);
+    Matrix VBED(3, 1);
+    Matrix SBII(3, 1);
+    Matrix VBII(3, 1);
+    double dvbi(0);
+    double psivdx(0);
+    double thtvdx(0);
+
+    double psibdx=kinematics->psibdx;
+    double thtbdx=kinematics->thtbdx;
+    double phibdx=kinematics->phibdx;
 
     Matrix VBEB(3,1);
     Matrix POLAR(3,1);
-    Matrix TBD(3,3);
     //Earth's angular velocity skew-symmetric matrix (3x3)
+    Matrix WEII(3,3);
     WEII.assign_loc(0,1,-WEII3);
     WEII.assign_loc(1,0,WEII3);
 
     //converting geodetic lonx, latx, alt to SBII
-    SBII=cad_in_geo84(lonx*RAD,latx*RAD,alt,time);
+    // XXX: time might skew
+    SBII=cad_in_geo84(lonx*RAD,latx*RAD,alt,get_rettime());
     dbi=SBII.absolute();
 
     //building geodetic velocity VBED(3x1) from  alpha, beta, and dvbe
@@ -37,13 +53,13 @@ void Newton::initialize_newton(Kinematics& kine){
     VBEB.build_vec3(vbeb1,vbeb2,vbeb3);
 
     //building TBD
-    TBD=mat3tr(psibdx*RAD,thtbdx*RAD,phibdx*RAD);
+    Matrix TBD=mat3tr(psibdx*RAD,thtbdx*RAD,phibdx*RAD);
     //Geodetic velocity
     VBED=~TBD*VBEB;
 
     //building inertial velocity
-    TDI=cad_tdi84(lonx*RAD,latx*RAD,alt,time);
-    TGI=cad_tgi84(lonx*RAD,latx*RAD,alt,time);
+    Matrix TDI=cad_tdi84(lonx*RAD,latx*RAD,alt,get_rettime());
+    Matrix TGI=cad_tgi84(lonx*RAD,latx*RAD,alt,get_rettime());
     VBII=~TDI*VBED+WEII*SBII;
     dvbi=VBII.absolute();
 
@@ -52,61 +68,48 @@ void Newton::initialize_newton(Kinematics& kine){
     psivdx=DEG*POLAR.get_loc(1,0);
     thtvdx=DEG*POLAR.get_loc(2,0);
 
-//Update Matrix variables
-    for(i=0;i<9;i++){
-        weii[i]=WEII.get_loc((int) i/3,i%3);
-    }
-    for(i=0;i<3;i++){
-        IPos[i]=SBII.get_loc(i,0);
-    }
-    for(i=0;i<3;i++){
-        IVel[i]=VBII.get_loc(i,0);
-    }
-    for(i=0;i<9;i++){
-        tbd[i]=TBD.get_loc((int) i/3,i%3);
-    }
-    for(i=0;i<3;i++){
-        vbed[i]=VBED.get_loc(i,0);
-    }
-    for(i=0;i<9;i++){
-        tdi[i]=TDI.get_loc((int) i/3,i%3);
-    }
-    for(i=0;i<9;i++){
-        tgi[i]=TGI.get_loc((int) i/3,i%3);
-    }
-
+    //Update Matrix variables
+    WEII.fill(weii);
+    SBII.fill(IPos);
+    VBII.fill(IVel);
+    VBED.fill(vbed);
+    TDI.fill(tdi);
+    TGI.fill(tgi);
 }
 
-void Newton::newton(double int_step, Kinematics& kine, Environment& Env, Propulsion& Prop, forces& forc){
-    Kinematics=&kine;
-    Environment=&Env;
-    Propulsion=&Prop;
-    forces=&forc;
+void Newton::newton(double int_step){
+    double lon(0);
+    double lat(0);
+
+    Matrix FSPB(3, 1);
+
+    Matrix TDI(3, 3);
+    Matrix TGI(3, 3);
+    Matrix WEII(3, 3);
+    TDI.build_mat33(tdi);
+    TGI.build_mat33(tgi);
+    WEII.build_mat33(weii);
+
+    Matrix SBII(3, 1);
+    Matrix VBII(3, 1);
+    Matrix ABII(3, 1);
+    SBII.build_vec3(IPos);
+    VBII.build_vec3(IVel);
+    ABII.build_vec3(IAccl);
 
     Matrix GRAVG(3,1);
+    GRAVG.build_vec3(environment->gravg);
+
     Matrix TBI(3,3);
     Matrix FAPB(3,1);
+    TBI.build_mat33(kinematics->tbi);
+    FAPB.build_vec3(forces->fapb);
+
     Matrix FAP(3,1);
-    Matrix TBD(3,3);
+    FAP.build_vec3(forces->fap);
 
-    SBII.build_vec3(IPos[0],IPos[1],IPos[2]);
-    VBII.build_vec3(IVel[0],IVel[1],IVel[2]);
-    ABII.build_vec3(IAccl[0],IAccl[1],IAccl[2]);
-    FSPB.build_vec3(fspb[0],fspb[1],fspb[2]);
-    TGI.build_mat33(tgi[0],tgi[1],tgi[2],tgi[3],tgi[4],tgi[5],tgi[6],tgi[7],tgi[8]);
-    GRAVG.build_vec3(Environment->gravg[0],Environment->gravg[1],Environment->gravg[2]);
-    TBI.build_mat33(Kinematics->tbi[0],Kinematics->tbi[1],Kinematics->tbi[2],Kinematics->tbi[3],Kinematics->tbi[4]
-                    ,Kinematics->tbi[5],Kinematics->tbi[6],Kinematics->tbi[7],Kinematics->tbi[8]);
-    FAPB.build_vec3(forces->fapb[0],forces->fapb[1],forces->fapb[2]);
-    FAP.build_vec3(forces->FAP[0],forces->FAP[1],forces->FAP[2]);
-    TVD.build_mat33(tvd[0],tvd[1],tvd[2],tvd[3],tvd[4],tvd[5],tvd[6],tvd[7],tvd[8]);
-    TBD.build_mat33(Kinematics->tbd[0],Kinematics->tbd[1],Kinematics->tbd[2],Kinematics->tbd[3],Kinematics->tbd[4],Kinematics->tbd[5]
-                    ,Kinematics->tbd[6],Kinematics->tbd[7],Kinematics->tbd[8]);
-
-    double grav=Environment->grav;
-    double vmass=Propulsion->vmass;
-
-
+    double grav=environment->grav;
+    double vmass=propulsion->vmass;
 
     FSPB=FAPB*(1./vmass);
     Matrix NEXT_ACC=~TBI*FSPB+~TGI*GRAVG;
@@ -120,13 +123,13 @@ void Newton::newton(double int_step, Kinematics& kine, Environment& Env, Propuls
     FAP=FAP*(1./vmass);
     aero_loss=aero_loss+FAP.absolute()*int_step;
 
-    cad_geo84_in(lon,lat,alt,SBII,time);
-    TDI=cad_tdi84(lon,lat,alt,time);
-    TGI=cad_tgi84(lon,lat,alt,time);
+    cad_geo84_in(lon,lat,alt,SBII,get_rettime());
+    TDI=cad_tdi84(lon,lat,alt,get_rettime());
+    TGI=cad_tgi84(lon,lat,alt,get_rettime());
     lonx=lon*DEG;
     latx=lat*DEG;
     //geographic velocity in geodetic axes VBED(3x1) and flight path angles
-    VBED=TDI*(VBII-WEII*SBII);
+    Matrix VBED=TDI*(VBII-WEII*SBII);
     Matrix POLAR=VBED.pol_from_cart();
     dvbe=POLAR[0];
     psivdx=DEG*POLAR[1];
@@ -135,7 +138,7 @@ void Newton::newton(double int_step, Kinematics& kine, Environment& Env, Propuls
     gravity_loss2=gravity_loss2+grav*sin(thtvdx*RAD)*int_step;
 
     //T.M. of geographic velocity wrt geodetic coordinates
-    TVD=mat2tr(psivdx*RAD,thtvdx*RAD);
+    Matrix TVD=mat2tr(psivdx*RAD,thtvdx*RAD);
 
     //diagnostics: acceleration achieved
     ayx=FSPB[1]/AGRAV;
@@ -148,30 +151,12 @@ void Newton::newton(double int_step, Kinematics& kine, Environment& Env, Propuls
     gndtrkmx=0.001*grndtrck;
     gndtrnmx=NMILES*grndtrck;
 
-    //update Martix variables
-    for(i=0;i<3;i++){
-        IPos[i]=SBII.get_loc(i,0);
-    }
-    for(i=0;i<3;i++){
-        IVel[i]=VBII.get_loc(i,0);
-    }
-    for(i=0;i<3;i++){
-        IAccl[i]=ABII.get_loc(i,0);
-    }
-    for(i=0;i<3;i++){
-        vbed[i]=VBED.get_loc(i,0);
-    }
-    for(i=0;i<3;i++){
-        fspb[i]=FSPB.get_loc(i,0);
-    }
-    for(i=0;i<9;i++){
-        tdi[i]=TDI.get_loc((int) i/3,i%3);
-    }
-    for(i=0;i<9;i++){
-        tgi[i]=TGI.get_loc((int) i/3,i%3);
-    }
-    for(i=0;i<9;i++){
-        tvd[i]=TVD.get_loc((int) i/3,i%3);
-    }
-
+    SBII.fill(IPos);
+    VBII.fill(IVel);
+    ABII.fill(IAccl);
+    VBED.fill(vbed);
+    FSPB.fill(fspb);
+    TDI.fill(tdi);
+    TGI.fill(tgi);
+    TVD.fill(tvd);
 }
