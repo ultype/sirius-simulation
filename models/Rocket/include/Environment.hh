cba4430fb89c6f9c512254c4a1f455c034020bb9
sonicyang <sonic.tw.tp@gmail.com>
2016-09-13 00:16:25 +0800

    [ENV] Add missing variable

--- models/Rocket/include/Environment.hh
+++ models/Rocket/include/Environment.hh
@@ -26,6 +26,9 @@ class Environment{
         double gravg[3];    /* *io (m/s2)       Gravity acceleration in geocentric coord */
         double grav;        /* *io (m/s2)       Magnitude of gravity acceleration */
         double dvae;        /* *io (m/s)        Madnitude of constant air speed */
+        double dvba;        /* *io (m/s)        Vehicle speed wrt air */
+
+        double vaed[3];     /* *io (m/s)        Smoothed wind velocity in geodetic coord */
 
 };
 
