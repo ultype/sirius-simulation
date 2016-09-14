7fa03b590c7724558cf0fa161517d1e4b76b81c9
sonicyang <sonic.tw.tp@gmail.com>
2016-09-14 02:31:15 -0700

    [INS] Ported ins module variable and Gyro, Accel, Grav Error input

--- RUN_test/input.py
+++ RUN_test/input.py
@@ -3,16 +3,16 @@
 # execfile("Modified_data/GPS_Satellite.dr")
 
 #SLV
-rkt.newton.lonx    = 121.1    #Vehicle longitude - deg  module newton
-rkt.newton.latx    = 22.68    #Vehicle latitude - deg  module newton
-rkt.newton.alt     = 100    #Vehicle altitude - m  module newton
-rkt.newton.dvbe    = 1    #Vehicle geographic speed - m/s  module newton
+rkt.newton.lonx       = 121.1    #Vehicle longitude - deg  module newton
+rkt.newton.latx       = 22.68    #Vehicle latitude - deg  module newton
+rkt.newton.alt        = 100    #Vehicle altitude - m  module newton
+rkt.newton.dvbe       = 1    #Vehicle geographic speed - m/s  module newton
 
-rkt.kinematics.phibdx   = 0    #Rolling angle of veh wrt geod coord - deg  module kinematics
-rkt.kinematics.thtbdx   = 90    #Pitching angle of veh wrt geod coord - deg  module kinematics
-rkt.kinematics.psibdx   = 90    #Yawing angle of veh wrt geod coord - deg  module kinematics
-rkt.newton.alpha0x = 0    #Initial angle-of-attack - deg  module newton
-rkt.newton.beta0x   = 0    #Initial sideslip angle - deg  module newton
+rkt.kinematics.phibdx = 0    #Rolling angle of veh wrt geod coord - deg  module kinematics
+rkt.kinematics.thtbdx = 90    #Pitching angle of veh wrt geod coord - deg  module kinematics
+rkt.kinematics.psibdx = 90    #Yawing angle of veh wrt geod coord - deg  module kinematics
+rkt.newton.alpha0x    = 0    #Initial angle-of-attack - deg  module newton
+rkt.newton.beta0x     = 0    #Initial sideslip angle - deg  module newton
 #environment
 
 #aerodynamics
@@ -20,19 +20,38 @@ rkt.newton.beta0x   = 0    #Initial sideslip angle - deg  module newton
 #propulsion
 
 #INS
+#INS Acceleration
+rkt.ins.efspb  = [0, 0, 0]
+rkt.ins.ewalka = [0, 0, 0]
+rkt.ins.emisa  = [0 ,0 ,0]  #gauss(0, 1.1e-4)
+rkt.ins.escala = [0, 0, 0]  #gauss(0, 5e-4)
+rkt.ins.ebiasa = [0, 0, 0]  #gauss(0, 3.56e-3)
+
+#ins gyrp
+rkt.ins.eug    = [0, 0, 0]
+rkt.ins.ewg    = [0, 0, 0]
+rkt.ins.ewbib  = [0, 0, 0]
+rkt.ins.ewalkg = [0, 0, 0]
+rkt.ins.eunbg  = [0, 0, 0]
+rkt.ins.emisg  = [0, 0, 0]  #gauss(0, 1.1e-4)
+rkt.ins.escalg = [0, 0, 0]  #gauss(0, 2.e-5)
+rkt.ins.ebiasg = [0, 0, 0]  #gauss(0, 1.e-6)
+
+#ins grav
+rkt.ins.egravi = [0, 0, 0]
 
 #GPS
 gps_sats.sats.almanac_time = 80000    #Time since almanac epoch at sim start - sec  module gps
 
-rkt.gpsr.del_rearth   = 2317000    #Delta to Earth's radius for GPS clear LOS signal reception - m  module gps
-rkt.gpsr.gps_acqtime  = 10    #Acquisition time for GPS signal - s  module gps
-rkt.gpsr.gps_step     = 0.1    #GPS update interval - s  module gps
+rkt.gpsr.del_rearth        = 2317000    #Delta to Earth's radius for GPS clear LOS signal reception - m  module gps
+rkt.gpsr.gps_acqtime       = 10    #Acquisition time for GPS signal - s  module gps
+rkt.gpsr.gps_step          = 0.1    #GPS update interval - s  module gps
 
-rkt.gpsr.ucfreq_noise = 0.1 #User clock frequency error - m/s MARKOV  module gps
-rkt.gpsr.ucbias_error = 0 #User clock bias error - m GAUSS  module gps
+rkt.gpsr.ucfreq_noise      = 0.1 #User clock frequency error - m/s MARKOV  module gps
+rkt.gpsr.ucbias_error      = 0 #User clock bias error - m GAUSS  module gps
 
-rkt.gpsr.PR_BIAS      = [0, 0, 0, 0] #Pseudo-range bias - m GAUSS  module gps
-rkt.gpsr.PR_NOISE     = [0.25, 0.25, 0.25, 0.25] #Pseudo-range noise - m MARKOV  module gps
-rkt.gpsr.DR_NOISE     = [0.03, 0.03, 0.03, 0.03] #Delta-range noise - m/s MARKOV  module gps
+rkt.gpsr.PR_BIAS           = [0, 0, 0, 0] #Pseudo-range bias - m GAUSS  module gps
+rkt.gpsr.PR_NOISE          = [0.25, 0.25, 0.25, 0.25] #Pseudo-range noise - m MARKOV  module gps
+rkt.gpsr.DR_NOISE          = [0.03, 0.03, 0.03, 0.03] #Delta-range noise - m/s MARKOV  module gps
 
 trick.stop(180)
--- models/Rocket/include/Ins.hh
+++ models/Rocket/include/Ins.hh
@@ -16,9 +16,66 @@ class INS {
     void default_data();
     void initialize();
 
-    double IPos[3];     /* *i (m)       Body in Inertial Coordinate */
-    double IVel[3];     /* *i (m)       Body in Inertial Coordinate */
-    double IW[3];       /* *i (m)       Body in Inertial Coordinate */
+
+
+    double IPos[3];     /* *i   (m)     Body in Inertial Coordinate */
+    double IVel[3];     /* *i   (m)     Body in Inertial Coordinate */
+    double IW[3];       /* *i   (m)     Body in Inertial Coordinate */
+
+    /* Accelmeter */
+    double efspb[3];    /* *i   (N/kg)  Error in specific force on body in body coordinate */
+    double ewalka[3];   /* *i   (m/s2)  Acceleration random noise */
+    double emisa[3];    /* *i   (r)     Acceleration misalignment */
+    double escala[3];   /* *i   (--)    Acceleration scale factor */
+    double ebiasa[3];   /* *i   (m/s2)  Acceleration bias */
+
+    /* gyro */
+    double eug[3];      /* *i   (r/s)   Gyro spin axis accel sensitivity */
+    double ewg[3];      /* *i   (r/s)   Gyro random walk errors */
+    double ewbib[3];    /* *i   (r/s)   Error in angular vel of body wrt earth */
+    double ewalkg[3];   /* *i   (r/s0.5) Random walk */
+    double eunbg[3];    /* *i   (r)     Gyro cluster misalignment */
+    double emisg[3]     /* *i   (r)     Gyro misalignmt */
+    double escalg[3]    /* *i   (--)    Gyro scale factor */
+    double ebiasg[3]    /* *i   (r/s)   Gyro bias */
+
+    /* grav */
+    double egravi[3];   /* *i   (--)    error by gravity */
+
+    int mins;           /* *io  (--)    INS mode. =0:ideal INS; =1:with INS error */
+    double frax_algnmnt /* *io  (--)    Fractn to mod initial INS err state: XXO=XXO(1+frax) */
+    double vbiic[3];    /* *io  (m/s)   Computed body vel in earth coor */
+    double sbiic[3];    /* *io  (m)     Computed pos of body wrt earth reference point*/
+    double wbici[3];    /* *io  (r/s)   Computed inertial body rate in inert coordinate */
+    double wbicb[3];    /* *io  (r/s)   Computed inertial body rate in body coordinate */
+    double tbic[3][3];  /* *io  (--)    Comp T.M. of body wrt earth coordinate */
+
+    double ppcx;        /* *io  (d/s)   INS computed roll rate */
+    double qqcx;        /* *io  (d/s)   INS computed pitch rate */
+    double rrcx;        /* *io  (d/s)   INS computed yaw rate */
+
+    double loncx;       /* *io  (d)     INS derived longitude */
+    double latcx;       /* *io  (d)     INS derived latitude */
+    double altc;        /* *io  (m)     INS derived altitude */
+    double vbecd[3];    /* *io  (m/s)   Geodetic velocity */
+    double dvbec;       /* *io  (m/s)   Computed body speed wrt earth */
+    double TDCI[3][3];  /* *io  (--)    Comp T.M. of geodetic wrt inertial */
+    double thtvdcx      /* *io  (d)     INS computed vertical flight path angle */
+    double psivdcx;     /* *io  (d)     INS computed heading angle */
+    double FSPCB[3];    /* *io  (N/kg)  Computed specific force on body */
+    double dbic;        /* *io  (m)     INS computed vehicle distance from Earth center */
+    double alphacx;     /* *io  (d)     INS computed angle of attack */
+    double phipcx;      /* *io  (d)     INS computed aero roll angle */
+    double ricicd[3];   /* *io  (r)     INS tilt error derivative */
+    double rici[3];     /* *io  (r)     INS tilt error */
+    double evbid[3];    /* *io  (m/s)   INS vel error derivative */
+    double evbi[3];     /* *io  (m/s)   INS vel error */
+    double esbid[3];    /* *io  (m)     INS pos error derivative */
+    double esbi[3];     /* *io  (m)     INS pos error */
+    double ins_pos_err; /* *io  (m)     INS absolute postion error */
+    double ins_vel_err; /* *io  (m/s)   INS absolute velocity error */
+    double ins_tilt_err;/* *io  (r)     INS absolute tilt error */
+
 };
 
 #endif  // __INS_HH__
