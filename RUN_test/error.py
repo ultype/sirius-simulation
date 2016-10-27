2ace0e048f9ea65f9b05b2f4bc4264d351851bdb
NekOrz <joe1256joe@gmail.com>
2016-10-27 16:24:13 +0800

    Finish Basic IO
    1. Finishing "all to all" and "last one only".
    
    2. Testing python-matplotlib

--- RUN_test/error.py
+++ RUN_test/error.py
@@ -1,5 +1,6 @@
 import csv
 import argparse
+import matplotlib
 
 def toFloat(str):
     try:
@@ -16,79 +17,55 @@ parser.add_argument('target', help='Input the file position of target file.')
 
 parser.add_argument('-l', '--last',
     help='Compare the last data between golden and target file.',
-    action='store_true'
+    action='store_true',
 )
 
 args = parser.parse_args()
 
 try:
-    with open(args.golden, 'rb') as goldenFile,            \
-         open(args.target, 'rb') as targetFile,            \
-         open('result.csv','wb') as outoutFile:
+    with open(args.golden, 'rb') as goldenFile, \
+         open(args.target, 'rb') as targetFile, \
+         open('result.csv','wb') as outputFile:
+
         goldenData = csv.reader(goldenFile, delimiter=',', quotechar='\n')
         targetData = csv.reader(targetFile, delimiter=',', quotechar='\n')
+        writer = csv.writer(outputFile, delimiter=',', quotechar='\n')
 
         goldenArray = [];
         targetArray = [];
         outputArray = [];
+        #Eat all shitz in
+        for row in goldenData:
+            goldenArray.append([]);
+            for num in row:
+                goldenArray[-1].append(toFloat(num))
 
-        if(args.last):
-            print('jizz')
-        else:
-            for row in goldenData:
-                goldenArray.append([]);
-                for num in row:
-                    goldenArray[-1].append(toFloat(num))
+        for row in targetData:
+            targetArray.append([]);
+            for num in row:
+                targetArray[-1].append(toFloat(num))
 
-            for row in targetData:
-                targetArray.append([]);
-                for num in row:
-                    targetArray[-1].append(toFloat(num))
-
-            for idxRow, goldenRow in enumerate(goldenData):
+        if(args.last): #only last
+            for idx, num in enumerate(goldenArray[-1]):
+                try:
+                    outputArray.append((num - targetArray[-1][idx])/num)
+                except TypeError:
+                    outputArray.append(goldenNum)
+                except ZeroDivisionError:
+                    outputArray.append('DivByZero')
+            writer.writerow(outputArray);
+        else: #All to All
+            for idxRow, goldenRow in enumerate(goldenArray):
                 outputArray.append([]);
                 for idxNum, goldenNum in enumerate(goldenRow):
                     try:
                         outputArray[-1].append(
                             (goldenNum - targetArray[idxRow][idxNum])/goldenNum
                         )
+                    except TypeError:
+                        outputArray[-1].append(goldenNum)
                     except ZeroDivisionError:
                         outputArray[-1].append('DivByZero')
-
-
-
+            writer.writerows(outputArray)
 except IOError as e:
     print(e)
-'''
-
-
-        data = csv.reader(csvfile, delimiter=',', quotechar='\n')
-
-        for row in data:
-            dataArray.append([]);
-            for e in row:
-                dataArray[-1].append(toFloat(e))
-
-        lastdataArray = [];
-        try:
-            with open('final.csv', 'rb') as lastfile:
-                data = csv.reader(lastfile, delimiter=',', quotechar='\n')
-
-                for row in data:
-                    for e in row:
-                        lastdataArray.append(toFloat(e))
-
-                for idx, last in enumerate(lastdataArray):
-                    try:
-                        print (dataArray[-1][idx] - last)/dataArray[-1][idx]
-                    except ZeroDivisionError:
-                        print 'Divided by zero'
-        except IOError:
-            print 'No final.csv, assuming starting for the first time...'
-
-        with open('final.csv', 'wb') as output:
-            writer = csv.writer(output, delimiter=',', quotechar='\n')
-            writer.writerow(dataArray[-1])
-except IOError:
-    print 'No log_rocket_csv.csv, please run input.py with rocket_csv.dr at least once.'
-'''
