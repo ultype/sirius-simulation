48f3b313bacda3443f2fd1725ccd2bf06d557b17
sonicyang <sonic.tw.tp@gmail.com>
2017-01-20 01:55:11 +0800

    Run trick-CP instead of make for building

--- tools/run_test.sh
+++ tools/run_test.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-make clean
-make -j16
+trick-CP
 ./S_main_Linux_5.4_x86_64.exe RUN_test/test.py
 python ./tools/generate_error.py RUN_golden/golden.csv RUN_test/log_rocket_csv.csv -l
 python ./tools/ci_test.py result.csv 1e-6
