ee61d5a402ac95fb6525ed4b6fbc9d220efd59f7
sonicyang <sonic.tw.tp@gmail.com>
2017-01-25 15:06:54 +0800

    Enable building with -j16

--- S_overrides.mk
+++ S_overrides.mk
@@ -1,3 +1,4 @@
 TRICK_CFLAGS += -I${TRICK_HOME}/trick_models -Imodels/includes -g
 TRICK_CXXFLAGS += -I${TRICK_HOME}/trick_models -Imodels/includes -g
 TRICK_USER_LINK_LIBS += -larmadillo
+MAKEFLAGS += -j16
