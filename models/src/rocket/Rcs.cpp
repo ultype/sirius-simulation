3ff467156f5a08a27c7a979af756afd696d56186
sonicyang <sonic.tw.tp@gmail.com>
2017-02-10 12:11:50 +0800

    Refactored rcs mode and rcs type

--- models/src/rocket/Rcs.cpp
+++ models/src/rocket/Rcs.cpp
@@ -31,6 +31,18 @@ void RCS::default_data(){
 void RCS::initialize(){
 }
 
+void RCS::enable_rcs(){
+    rcs_type = ON_OFF_RCS;
+}
+
+void RCS::disable_rcs(){
+    rcs_type = NO_RCS;
+}
+
+void RCS::set_mode(enum RCS::RCS_MODE in){
+    rcs_mode = in;
+}
+
 ///////////////////////////////////////////////////////////////////////////////
 // RCS thruster module
 // Member function of class 'Hyper'
@@ -56,10 +68,8 @@ void RCS::actuate(){
 
     Matrix UTBC = guidance->get_UTBC();
 
-    //-------------------------------------------------------------------------
-    // decoding rcs_mom flag
-    int rcs_type = mrcs_moment / 10;
-    int rcs_mode = (mrcs_moment % 10);
+    if(this->rcs_type == NO_RCS) return;
+
 
     // on-off moment thrusters (Schmitt trigger)
 
@@ -90,34 +100,34 @@ void RCS::actuate(){
     }
 
     // moments generated about the three principle axes wrt the c.m
-    FMRCS[0]=o_roll*roll_mom_max;
-    FMRCS[1]=o_pitch*pitch_mom_max;
-    FMRCS[2]=o_yaw*yaw_mom_max;
-    FMRCS[0]=o_roll*rcs_thrust*rocket_r;
-    FMRCS[1]=o_pitch*rcs_thrust*(xcg-1);
-    FMRCS[2]=o_yaw*rcs_thrust*(xcg-1);
+    //FMRCS[0]=o_roll*roll_mom_max;
+    //FMRCS[1]=o_pitch*pitch_mom_max;
+    //FMRCS[2]=o_yaw*yaw_mom_max;
+    //FMRCS[0]=o_roll*rcs_thrust*rocket_r;
+    //FMRCS[1]=o_pitch*rcs_thrust*(xcg-1);
+    //FMRCS[2]=o_yaw*rcs_thrust*(xcg-1);
 
     //on-off side force thrusters (Schmitt trigger)
 
     //acceleration control
-    double ay=FSPCB[1];
-    e_right=acc_gain*(aycomx*AGRAV-ay);
+    //double ay=FSPCB[1];
+    //e_right=acc_gain*(aycomx*AGRAV-ay);
 
-    int o_right_save=o_right;
-    o_right=rcs_schmitt(e_right,right_save,dead_zone,hysteresis);
-    right_save=e_right;
-    if(o_right!=o_right_save)right_count++;
+    //int o_right_save=o_right;
+    //o_right=rcs_schmitt(e_right,right_save,dead_zone,hysteresis);
+    //right_save=e_right;
+    //if(o_right!=o_right_save)right_count++;
 
-    double az=FSPCB[2];
-    e_down=acc_gain*(azcomx*AGRAV-az);
-    int o_down_save=o_down;
-    o_down=rcs_schmitt(e_down,down_save,dead_zone,hysteresis);
-    down_save=e_down;
-    if(o_down!=o_down_save)down_count++;
+    //double az=FSPCB[2];
+    //e_down=acc_gain*(azcomx*AGRAV-az);
+    //int o_down_save=o_down;
+    //o_down=rcs_schmitt(e_down,down_save,dead_zone,hysteresis);
+    //down_save=e_down;
+    //if(o_down!=o_down_save)down_count++;
 
-    FARCS[0]=0;
-    FARCS[1]=o_right*side_force_max;
-    FARCS[2]=o_down*side_force_max;
+    //FARCS[0]=0;
+    //FARCS[1]=o_right*side_force_max;
+    //FARCS[2]=o_down*side_force_max;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -225,10 +235,8 @@ void RCS::set_hysteresis(double in) { hysteresis = in; }
 void RCS::set_rcs_thrust(double in) { rcs_thrust = in; }
 void RCS::set_rocket_r(double in) { rocket_r = in; }
 void RCS::set_rcs_pos(double in) { rcs_pos = in; }
-void RCS::set_mrcs_moment(int in) { mrcs_moment = in; }
 
-int RCS::get_mrcs_moment() { return mrcs_moment; }
-int RCS::get_mrcs_force() { return mrcs_force; }
+enum RCS::RCS_MODE RCS::get_rcs_mode() { return rcs_mode; }
 
 Matrix RCS::get_FMRCS() {
     Matrix FMRCS(3, 1);
