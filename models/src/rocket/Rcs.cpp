ae7751d2b1e2f0c7eebf68e7bc47fe742ada8af4
sonicyang <sonic.tw.tp@gmail.com>
2017-03-01 15:54:51 +0800

    remove not functional commented code in RCS

--- models/src/rocket/Rcs.cpp
+++ models/src/rocket/Rcs.cpp
@@ -132,61 +132,7 @@ void RCS::actuate(){
 
     }
 
-    // moments generated about the three principle axes wrt the c.m
-    //FMRCS[0]=o_roll*roll_mom_max;
-    //FMRCS[1]=o_pitch*pitch_mom_max;
-    //FMRCS[2]=o_yaw*yaw_mom_max;
-    //FMRCS[0]=o_roll*rcs_thrust*rocket_r;
-    //FMRCS[1]=o_pitch*rcs_thrust*(xcg-1);
-    //FMRCS[2]=o_yaw*rcs_thrust*(xcg-1);
-
-    //on-off side force thrusters (Schmitt trigger)
-
-    //acceleration control
-    //double ay=FSPCB[1];
-    //e_right=acc_gain*(aycomx*AGRAV-ay);
-
-    //int o_right_save=o_right;
-    //o_right=rcs_schmitt(e_right,right_save,dead_zone,hysteresis);
-    //right_save=e_right;
-    //if(o_right!=o_right_save)right_count++;
-
-    //double az=FSPCB[2];
-    //e_down=acc_gain*(azcomx*AGRAV-az);
-    //int o_down_save=o_down;
-    //o_down=rcs_schmitt(e_down,down_save,dead_zone,hysteresis);
-    //down_save=e_down;
-    //if(o_down!=o_down_save)down_count++;
-
-    //FARCS[0]=0;
-    //FARCS[1]=o_right*side_force_max;
-    //FARCS[2]=o_down*side_force_max;
 }
-
-///////////////////////////////////////////////////////////////////////////////
-// Proportional thrusters with limiters
-// Member function of class 'Hyper'
-//
-// parameter input
-//    input = commanded input - Nm
-//    limiter = max and min value of applied moment - Nm
-// return output
-//    output = moment about principle axis through c.m. - Nm
-//
-// 040309 Created by Peter H Zipfel
-///////////////////////////////////////////////////////////////////////////////
-/**************************************************
-double Hyper::rcs_prop(double input,double limiter)
-{
-    //local variable
-    double output(0);
-    output=input;
-    if(fabs(input)>limiter)
-        output=limiter*sign(input);
-
-    return output;
-}
-****************************************************/
 
 ///////////////////////////////////////////////////////////////////////////////
 // On-off thrusters modeled by the Schmitt trigger
@@ -212,6 +158,18 @@ double Hyper::rcs_prop(double input,double limiter)
 //////////////////////////////////////////////////////////////////////////////
 
 void RCS::rcs_schmitt_thrust() {
+    /* ------------------------------------------------------------ /
+    / moments generated about the three principle axes wrt the c.m  /
+    / Equations:                                                    /
+    / FMRCS[0] = o_roll*roll_mom_max;                               /
+    / FMRCS[1] = o_pitch*pitch_mom_max;                             /
+    / FMRCS[2] = o_yaw*yaw_mom_max;                                 /
+    /                                                               /
+    / FMRCS[0] = o_roll*rcs_thrust*rocket_r;                        /
+    / FMRCS[1] = o_pitch*rcs_thrust*(xcg-1);                        /
+    / FMRCS[2] = o_yaw*rcs_thrust*(xcg-1);                          /
+    / ------------------------------------------------------------ */
+
     int o_roll_save = o_roll;
     o_roll = roll_schi.trigger(e_roll);
     if (o_roll != o_roll_save) roll_count++;
