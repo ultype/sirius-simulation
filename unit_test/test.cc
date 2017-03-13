44736db45ab2bf7ffe4cb11f790d8213830427f4
NekOrz <joe1256joe@gmail.com>
2017-03-14 00:40:06 +0800

    Updated on unit test

--- unit_test/test.cc
+++ unit_test/test.cc
@@ -1,12 +1,25 @@
 #include "../models/includes/rocket/Euler.hh"
 #include "gtest/gtest.h"
 
+#include "math/utility.hh"
+#include "math/integrate.hh"
+#include "math/matrix/utility.hh"
+
 class Kinematics;
 class Propulsion;
 class Forces;
 
+class Fake : public Kinematics {
+	public:
+		arma::mat get_TBI() {
+			arma::mat dum;
+			return dum;
+		}
+};
+
 TEST(EulerTest, General) {
-    _Euler_ euler(*(Kinematics*)NULL, *(Propulsion*)NULL, *(Forces*)NULL);
+    Fake *dum;
+    _Euler_ euler(*(Kinematics*)dum, *(Propulsion*)NULL, *(Forces*)NULL);
     euler.load_angular_velocity(0.1f,0.1f,0.1f);
     EXPECT_EQ(0.1f ,euler.get_ppx());
     EXPECT_EQ(0.1f ,euler.get_qqx());
