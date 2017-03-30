af951628f94767563d1394b42777d014cbae2cbc
Lai Chun Hsu <junxu@tispace.com>
2017-03-30 14:37:13 +0800

    Real time test

--- exe/single-node/RUN_test/golden-20170327-rcs-fc.py
+++ exe/single-node/RUN_test/golden-20170327-rcs-fc.py
@@ -3,19 +3,19 @@
 execfile("Modified_data/test.dr")
 execfile("Modified_data/gps.dr")
 # execfile("Modified_data/wangwang.dr")
-# trick.exec_set_enable_freeze(True)
-# trick.exec_set_freeze_command(True)
-# trick.sim_control_panel_set_enabled(True)
+#trick.exec_set_enable_freeze(True)
+#trick.exec_set_freeze_command(True)
+#trick.sim_control_panel_set_enabled(True)
 ##########################################################
 
 #############################################################
 #Realtime setting
-# trick.exec_set_thread_cpu_affinity(0,0)
-# trick.exec_set_thread_priority(0,1)
-# trick.frame_log_on()
-# trick.real_time_enable()
-# trick.exec_set_software_frame(0.01)
-# trick.itimer_enable()
+#trick.exec_set_thread_cpu_affinity(0,1)
+#trick.exec_set_thread_priority(0,1)
+#trick.frame_log_on()
+#trick.real_time_enable()
+#trick.exec_set_software_frame(0.001)
+#trick.itimer_enable()
 ##############################################################
 
 ##############################################################
