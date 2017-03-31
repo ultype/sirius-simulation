f70efd1e15300165b7d01e3c2ed33201e9f5793a
Che-Kang Wu <erickitten@gmail.com>
2017-03-31 16:53:57 +0800

    Fix test.sh path error & use python2

--- exe/SIL/test.sh
+++ exe/SIL/test.sh
@@ -3,5 +3,5 @@ set -e
 
 trick-CP
 ./S_main_Linux_5.4_x86_64.exe RUN_golden/golden-20170327-gps.py
-python ../../tools/generate_error.py RUN_golden/golden-20170327-gps.csv RUN_golden/log_rocket_csv.csv -l
-python ../../tools/ci_test.py result.csv 1e-5
+python2 ../../tools/generate_error.py RUN_golden/golden-20170327-gps.csv RUN_golden/log_rocket_csv.csv -l
+python2 ../../tools/ci_test.py result.csv 1e-5
