be8aa4db7e5983c5ecb9aaceec30d6805e1990ad
Chung-Fan <sonicyang@mail.tainan.tispace.com>
2017-04-09 17:48:31 +0800

    Remove ui code in realtime.py

--- public/Modified_data/realtime.py
+++ public/Modified_data/realtime.py
@@ -2,7 +2,3 @@
 trick.real_time_enable()
 trick.exec_set_software_frame(0.05)
 trick.itimer_enable()
-
-trick.exec_set_enable_freeze(True)
-trick.exec_set_freeze_command(True)
-trick.sim_control_panel_set_enabled(True)
