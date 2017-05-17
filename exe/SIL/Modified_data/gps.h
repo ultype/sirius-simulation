8fc8b67cd5dc8f7d2c170ff32096bfc18fefe016
sonicyang <sonic.tw.tp@gmail.com>
2017-05-17 15:29:05 +0800

    Fix coding style of includsion guard

--- exe/SIL/Modified_data/gps.h
+++ exe/SIL/Modified_data/gps.h
@@ -1,5 +1,5 @@
-#ifndef EXE_PIL_MASTER_MODIFIED_DATA_GPS_H_
-#define EXE_PIL_MASTER_MODIFIED_DATA_GPS_H_
+#ifndef EXE_SIL_MODIFIED_DATA_GPS_H_
+#define EXE_SIL_MODIFIED_DATA_GPS_H_
 
 #include "trick/DRAscii.hh"
 #include "trick/DataRecordGroup.hh"
@@ -39,4 +39,4 @@ extern "C" void record_gps() {
     drg->enable();
 }
 
-#endif  // EXE_PIL_MASTER_MODIFIED_DATA_GPS_H_
+#endif  // EXE_SIL_MODIFIED_DATA_GPS_H_
