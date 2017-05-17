24a55c980178bda0645523c09289b0f44c0cfff4
NekOrz <joe1256joe@gmail.com>
2017-05-17 20:03:49 +0800

    Unit test: The rest of the timeTest

--- unit_test/test.cc
+++ unit_test/test.cc
@@ -49,7 +49,7 @@ TEST(timeTest, UTCtoOther) {
     time_util::Modified_julian_date jul(utc);
 
     EXPECT_EQ(1949, gps.get_week());
-    EXPECT_EQ(73220 + 18, gps.get_SOW());
+    EXPECT_DOUBLE_EQ(73220 + 18, gps.get_SOW());
 
     EXPECT_NEAR(57887.8474537, jul.get_mjd(), 1e-8);
     /* Not yet figured
@@ -60,4 +60,45 @@ TEST(timeTest, UTCtoOther) {
     EXPECT_EQ(1, cuc.get_F1());
     EXPECT_EQ(1, cuc.get_F2());
     */
+}
+
+TEST(timeTest, GPStoOther) {
+    time_util::GPS_TIME gps;
+
+    gps.set_week(1949);
+    gps.set_SOW(73220 + 18);
+
+    EXPECT_EQ(1494793220, gps.to_unix());
+
+    time_util::UTC_TIME utc(gps);
+    time_util::CCSDS_CUC cuc(gps);
+    time_util::Modified_julian_date jul(gps);
+
+    EXPECT_EQ(2017, utc.get_year());
+    EXPECT_EQ(5, utc.get_month());
+    EXPECT_EQ(14, utc.get_day());
+    EXPECT_EQ(20, utc.get_hour());
+    EXPECT_EQ(20, utc.get_min());
+    EXPECT_DOUBLE_EQ(20, utc.get_sec());
+
+    EXPECT_NEAR(57887.8474537, jul.get_mjd(), 1e-8);
+}
+
+TEST(timeTest, JuliantoOther) {
+    time_util::Modified_julian_date jul;
+
+    jul.set_mjd(57887.8474537);
+
+    time_util::GPS_TIME gps(jul);
+    time_util::UTC_TIME utc(jul);
+
+    EXPECT_EQ(1949, gps.get_week());
+    EXPECT_NEAR(73220 + 18, gps.get_SOW(), 1e-3);
+
+    EXPECT_EQ(2017, utc.get_year());
+    EXPECT_EQ(5, utc.get_month());
+    EXPECT_EQ(14, utc.get_day());
+    EXPECT_EQ(20, utc.get_hour());
+    EXPECT_EQ(20, utc.get_min());
+    EXPECT_NEAR(20, utc.get_sec(), 1e-3);
 }
