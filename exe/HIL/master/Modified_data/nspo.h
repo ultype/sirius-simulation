38dde0cc8e5f1adbc43bd96dd376b7080d42b884
Dung-Ru Tsai <dungrutsai@tispace.com>
2017-12-06 19:00:19 +0800

    HIL Simulation Create

---
+++ exe/HIL/master/Modified_data/nspo.h
@@ -0,0 +1,36 @@
+#ifndef EXE_HIL_MASTER_MODIFIED_DATA_NSPO_H_
+#define EXE_HIL_MASTER_MODIFIED_DATA_NSPO_H_
+
+#include "trick/DRAscii.hh"
+#include "trick/DataRecordGroup.hh"
+#include "trick/data_record_proto.h"
+
+extern "C" void record_nspo() {
+    Trick::DRAscii *drg = new Trick::DRAscii("nspo");
+    drg->set_freq(Trick::DR_Always);
+    drg->set_cycle(0.001);
+    drg->set_single_prec_only(false);
+    drg->add_variable("rkt.time->gpstime.SOW");
+    drg->add_variable("rkt.newton._SBEE[0]");
+    drg->add_variable("rkt.newton._SBEE[1]");
+    drg->add_variable("rkt.newton._SBEE[2]");
+    drg->add_variable("rkt.newton._VBEE[0]");
+    drg->add_variable("rkt.newton._VBEE[1]");
+    drg->add_variable("rkt.newton._VBEE[2]");
+    drg->add_variable("rkt.newton._ABEE[0]");
+    drg->add_variable("rkt.newton._ABEE[1]");
+    drg->add_variable("rkt.newton._ABEE[2]");
+    drg->add_variable("rkt.newton._JBEE[0]");
+    drg->add_variable("rkt.newton._JBEE[1]");
+    drg->add_variable("rkt.newton._JBEE[2]");
+    drg->add_variable("rkt.kinematics.psibd");
+    drg->add_variable("rkt.kinematics.thtbd");
+    drg->add_variable("rkt.kinematics.phibd");
+    drg->add_variable("rkt.euler._WBEB[0]");
+    drg->add_variable("rkt.euler._WBEB[1]");
+    drg->add_variable("rkt.euler._WBEB[2]");
+    add_data_record_group(drg, Trick::DR_Buffer);
+    drg->enable();
+}
+
+#endif  // EXE_HIL_MASTER_MODIFIED_DATA_NSPO_H_
---
+++ exe/HIL/master/S_overrides.mk
@@ -0,0 +1,13 @@
+INCLUDES = -I${TRICK_HOME}/trick_models \
+		   -I../../../models/gnc/include \
+		   -I../../../models/dm/include \
+		   -I../../../models/cad/include \
+		   -I../../../models/math/include \
+		   -I../../../models/aux/include \
+		   -I../../../models/sensor/include \
+		   -I../../../models/driver/include
+
+TRICK_CFLAGS += --std=c++11 ${INCLUDES} -g
+TRICK_CXXFLAGS += --std=c++11 ${INCLUDES} -g
+TRICK_USER_LINK_LIBS += -larmadillo -lboost_serialization
+MAKEFLAGS += -j16
