2c0cb1e2c576babb1e4f94a9a02fa8e66e388351
Dung-Ru Tsai <dungrutsai@tispace.com>
2017-12-14 16:02:08 +0800

    Add the HIL compile flag

--- exe/HIL/master/S_overrides.mk
+++ exe/HIL/master/S_overrides.mk
@@ -15,9 +15,9 @@ INCLUDES = -I${TRICK_HOME}/trick_models \
 		   -I$(SIRIUS_HOME)/models/driver/include \
 		   -I$(SIRIUS_HOME)/models/icf/include
 
-TRICK_CFLAGS += --std=c++11 ${INCLUDES} -g -D_GNU_SOURCE
+TRICK_CFLAGS += --std=c++11 ${INCLUDES} -g -D_GNU_SOURCE -DCONFIG_HIL_ENABLE
 TRICK_CFLAGS += -Wall -Wmissing-prototypes -Wextra -Wshadow
-TRICK_CXXFLAGS += --std=c++11 ${INCLUDES} -g
+TRICK_CXXFLAGS += --std=c++11 ${INCLUDES} -g -DCONFIG_HIL_ENABLE
 TRICK_CXXFLAGS += -Wall -Wextra -Wshadow
 TRICK_USER_LINK_LIBS += -larmadillo -lboost_serialization
 MAKEFLAGS += -j16
