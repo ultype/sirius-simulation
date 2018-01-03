e9b12dc0b34e532b1a6962844ca9931ef40bcb2d
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-01-03 10:44:50 +0800

    Ftrace script: Default use the empty function tracer

--- models/icf/QA_Tool/ftrace_script/ftrace_setup.sh
+++ models/icf/QA_Tool/ftrace_script/ftrace_setup.sh
@@ -8,26 +8,40 @@ echo 65535 > /sys/kernel/debug/tracing/buffer_size_kb
 echo "mono" > /sys/kernel/debug/tracing/trace_clock
 
 # sh -c 'sudo echo $$ > /sys/kernel/debug/tracing/set_ftrace_pid;sudo echo 1 > /sys/kernel/debug/tracing/tracing_on; sudo ./readCan'
-#echo function > /sys/kernel/debug/tracing/current_tracer
 #echo "32640" > /sys/kernel/debug/tracing/set_ftrace_pid
 
 ##### Function Tracer #####
 echo function > /sys/kernel/debug/tracing/current_tracer
-echo :mod:adv17v35x > /sys/kernel/debug/tracing/set_ftrace_filter
-echo :mod:pcan > /sys/kernel/debug/tracing/set_ftrace_filter
-echo serialadv_start_tx > /sys/kernel/debug/tracing/set_ftrace_filter
-echo pcan_pci_irqhandler >> /sys/kernel/debug/tracing/set_ftrace_filter
+#echo :mod:adv17v35x > /sys/kernel/debug/tracing/set_ftrace_filter
+#echo :mod:pcan > /sys/kernel/debug/tracing/set_ftrace_filter
+#echo serialadv_start_tx > /sys/kernel/debug/tracing/set_ftrace_filter
+#echo pcan_pci_irqhandler >> /sys/kernel/debug/tracing/set_ftrace_filter
+##### Empty Function #####
+echo bnep_net_setup > /sys/kernel/debug/tracing/set_ftrace_filter
 cat /sys/kernel/debug/tracing/set_ftrace_filter
 
 ##### Event tracer #####
-#IRQ PCAN
+##IRQ PCAN
 #echo nop > /sys/kernel/debug/tracing/current_tracer
 #echo "irq==16" > /sys/kernel/debug/tracing/events/irq/irq_handler_entry/filter
 #echo 1 > /sys/kernel/debug/tracing/events/irq/irq_handler_entry/enable
-# system call
+
+##### System call #####
 echo "id>=500" > /sys/kernel/debug/tracing/events/raw_syscalls/sys_enter/filter
 echo 1 > /sys/kernel/debug/tracing/events/raw_syscalls/sys_enter/enable
 
 #Use trace-cmd
 #sudo trace-cmd record -p nop -e irq_handler_entry -f "irq==16" -e sys_enter -f "id>=500"
 #trace-cmd report
+
+##### Socket CAN Interface Enable #####
+# sudo ip link set can0 up type can bitrate 125000
+# sudo ip link set can1 up type can bitrate 125000
+
+##### CPU Isolation #####
+# sudo vim /etc/default/grub ===> add GRUB_CMDLINE_LINUX_DEFAULT="isolcpus=1"
+# sudo update-grub
+# sudo reboot
+
+##### Loogin Permission #####
+# sudo vim /etc/ssh/sshd_config ===> modify "PermitRootLogin yes"
