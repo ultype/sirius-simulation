5858981cc582393790f1fa30e508ac250db6eb2b
Dung-Ru <tsaidungru.ncku@gmail.com>
2018-02-21 19:45:07 +0800

    Test: Jenkins

--- exe/SIL/master/SIL.sh
+++ exe/SIL/master/SIL.sh
@@ -20,6 +20,5 @@ cd ../master
 python $SIRIUS_HOME_PATH/tools/generate_error.py $SIRIUS_HOME_PATH/public/golden.csv $SIL_MASTER_PATH/RUN_golden/log_rocket_csv.csv -l
 python $SIRIUS_HOME_PATH/tools/ci_test.py $SIL_MASTER_PATH/result.csv 1e-5 | tee test_result
 
-killall S_main*
 # Test the exit status of the command before pipe
 test ${PIPESTATUS[0]} -eq 0
