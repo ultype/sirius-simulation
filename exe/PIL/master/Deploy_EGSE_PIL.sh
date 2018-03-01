84831c5b12cdbd0b33cbb896a504f2f6c624c5f9
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-03-01 17:31:44 +0800

    PIL: Modify the PIL pass criteria 0.5

--- exe/PIL/master/Deploy_EGSE_PIL.sh
+++ exe/PIL/master/Deploy_EGSE_PIL.sh
@@ -31,7 +31,7 @@ sed_ipaddr_subst() {
 trick-CP
 ./S_main_Linux_5.4_x86_64.exe RUN_golden/golden.cpp
 python $SIRIUS_HOME_PATH/tools/generate_error.py $SIRIUS_HOME_PATH/public/golden.csv $PIL_MASTER_PATH/RUN_golden/log_rocket_csv.csv -l
-python $SIRIUS_HOME_PATH/tools/ci_test.py $PIL_MASTER_PATH/result.csv 1e-5 | tee test_result
+python $SIRIUS_HOME_PATH/tools/ci_test.py $PIL_MASTER_PATH/result.csv 5e-1 | tee test_result
 
 # Test the exit status of the command before pipe
 test ${PIPESTATUS[0]} -eq 0
