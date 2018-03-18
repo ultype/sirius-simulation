ac7e90f1b933a05664779ee5e04dcb58d855ea70
Dung-Ru <tsaidungru.ncku@gmail.com>
2018-03-18 19:09:31 +0800

    Add the install driver script condition

--- models/device_driver_3rd_party/adv_gpio_pci1737u/build_install.sh
+++ models/device_driver_3rd_party/adv_gpio_pci1737u/build_install.sh
@@ -5,10 +5,16 @@
 SCRIPT_PATH=$PWD
 DRVER_SOURCE=linux_driver_source_3.2.11.0_64bit
 PRODUCT=pci1737_pci1739
+BLACKLIST=$(grep "blacklist adv_pci_dio" /etc/modprobe.d/blacklist.conf)
+AUTOLOAD_DRIVERLIST=$(grep "biokernbase" /etc/modules)
 sudo cp /etc/modprobe.d/blacklist.conf  /etc/modprobe.d/blacklist.conf.bak
-echo "blacklist adv_pci_dio" | sudo tee -a /etc/modprobe.d/blacklist.conf
-echo "blacklist comedi" | sudo tee -a /etc/modprobe.d/blacklist.conf
-echo "blacklist adv_pci1710" | sudo tee -a /etc/modprobe.d/blacklist.conf
+
+if [ -z "$BLACKLIST" ]; then
+    echo "blacklist adv_pci_dio" | sudo tee -a /etc/modprobe.d/blacklist.conf
+    echo "blacklist comedi" | sudo tee -a /etc/modprobe.d/blacklist.conf
+    echo "blacklist adv_pci1710" | sudo tee -a /etc/modprobe.d/blacklist.conf
+fi
+
 
 #echo "blacklist bio1730" | sudo tee -a /etc/modprobe.d/blacklist.conf
 # Build kernel module
@@ -25,9 +31,11 @@ cd $SCRIPT_PATH/$DRVER_SOURCE/drivers/bin
 sudo cp *.ko /lib/modules/$(uname -r)/biodaq
 sudo depmod
 # Auto load the drver
-sudo echo "loop" | sudo tee -a /etc/modules
-sudo echo "biokernbase" | sudo tee -a /etc/modules
-sudo echo "$PRODUCT" | sudo tee -a /etc/modules
+if [ -z "$AUTOLOAD_DRIVERLIST" ]; then
+    sudo echo "loop" | sudo tee -a /etc/modules
+    sudo echo "biokernbase" | sudo tee -a /etc/modules
+    sudo echo "$PRODUCT" | sudo tee -a /etc/modules
+fi
 # Copy all dynamic lib to /usr/lib
 
 sudo cp $SCRIPT_PATH/$DRVER_SOURCE/libs/* /usr/lib/
