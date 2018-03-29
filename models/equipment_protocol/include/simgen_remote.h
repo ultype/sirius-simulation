c8d83c017842c8b447371e26b2830b2042f31191
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-03-29 12:51:08 +0800

    Implment the GPSRF Eumlator utility and command format

--- models/equipment_protocol/include/simgen_remote.h
+++ models/equipment_protocol/include/simgen_remote.h
@@ -2,11 +2,53 @@
 #define MODELS_EQUIPMENT_PROTOCOL_INCLUDE_SIMGEN_REMOTE_H_
 
 #include <stdint.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/socket.h>
 #include "simgen_udp_rx_commands.h"
-#define SIMGEN_IP "140.110.227.10"
+//#define SIMGEN_IP "140.110.227.10"
+#define SIMGEN_IP "127.0.0.1"
 #define SIMGEN_PORT 15650
 
+#define VEH_MOT(id) "v"#id"_m1"
 
+typedef enum _REMOTE_MOTION_CMD_ENUM {
+    REMOTE_MOTION_CMD_MOT = 0,
+    REMOTE_MOTION_CMD_MOTB,
+    REMOTE_MOTION_CMD_AIDING_OFFSET,
+    REMOTE_MOTION_CMD_MAX_NUM
+} REMOTE_MOTION_CMD_ENUM;
+
+struct simgen_eqmt_info_t {
+    int remote_cmd_channel_fd;
+    int udp_cmd_channel_fd;
+    struct sockaddr_in remote_cmd_addr;
+    struct sockaddr_in udp_cmd_addr;
+};
+
+struct simgen_remote_timestamp_t {
+    uint8_t day;
+    uint8_t hour;
+    uint8_t minute;
+    uint8_t second;
+    uint32_t milli;
+}
+
+struct simgen_remote_motion_mot_cmd_t {
+    struct simgen_remote_timestamp_t ts;
+    uint8_t cmd_idx;
+    uint8_t vehicle_id;
+    double position_xyz[3];         // uint: metres
+    double velocity_xyz[3];         // uint: m/s
+    double acceleration_xyz[3];     // uint: m/s^2
+    double jerk_xyz[3];             // uint: m/s^3
+    double heading;                 // uint range +/- pi
+    double elevation;               // uint range +/- pi/2
+    double bank;                    // uint range +/- pi
+    double angular_velocity[3];     // uint: rad/s
+    double angular_acceleration[3]; // uint: rad/s^2
+    double angular_jerk[3];         // uint: rad/s^3
+}
 
 #ifdef __cplusplus
 extern "C" {
--- models/equipment_protocol/src/simgen_remote.c
+++ models/equipment_protocol/src/simgen_remote.c
@@ -1,5 +1,6 @@
 #include "simgen_remote.h"
 
+
 char *nRU = "RU\n";
 char *nTR = "TR, 0\n";
 char *nAR = "AR\n";
@@ -20,6 +21,153 @@ char *t0 = "0 00:00:00.0000,mot,v1_m1, 4288466.6747904532, -2873655.5571095324, 
             0.0,0.0,0.0\n";
 char tn[512];
 char tcp_ack_buff[512];
+
+struct simgen_eqmt_info_t simgen_eqmt;
+
+static const char  *g_motion_cmd_list[3] = {"MOT", "MOTB", "AIDING_OFFSET"}
+static const char nRU[] = "RU\n";
+static const char nTR[] = "TR, 0\n";
+static const char nAR[] = "AR\n";
+static const char nSET_TIOP[] = "SET_TIOP,0,2,GATED,1PPS\n";
+static const char nEN[] = "-,EN,1\n";
+int simgen_motion_remote_cmd_gen(void *data, char *outbuff) {
+    struct simgen_remote_motion_cmd_t *motion_info = data;
+    char veh_mot_str[32];
+    strncpy(veh_mot_str, VEH_MOT(1), 32);
+    sprintf(outbuff, "%1d %2d:%2d:%2d.%3d,\
+        %s,%s,\
+        %20.10f, %20.10f, %20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f,\
+        %20.10f,%20.10f,%20.10f\n",
+        motion_info->ts.day, motion_info->ts.hour, motion_info->ts.minute, motion_info->ts.second, motion_info->ts.milli,
+        g_motion_cmd_list[motion_info->cmd_idx], veh_mot_str,
+        motion_info->position_xyz[0], motion_info->position_xyz[1], motion_info->position_xyz[2],
+        motion_info->velocity_xyz[0], motion_info->velocity_xyz[1], motion_info->velocity_xyz[2],
+        motion_info->acceleration_xyz[0], motion_info->acceleration_xyz[1], motion_info->acceleration_xyz[2],
+        motion_info->jerk_xyz[0], motion_info->jerk_xyz[1], motion_info->jerk_xyz[2],
+        motion_info->heading, motion_info->elevation, motion_info->bank,
+        motion_info->angular_velocity[0], motion_info->angular_velocity[1], motion_info->angular_velocity[2],
+        motion_info->angular_acceleration[0], motion_info->angular_acceleration[1], motion_info->angular_acceleration[2],
+        motion_info->angular_jerk[0], motion_info->angular_jerk[1], motion_info->angular_jerk[2]);
+    return 0;
+}
+
+
+int simgen_create_remote_cmd_channel(struct simgen_eqmt_info_t *eqmt_info, char *ifname, int net_port) {
+    int err;
+    int retry_cnt = 0;
+    if ((eqmt_info->remote_cmd_channel_fd = socket(AF_INET , SOCK_STREAM , 0)) < 0) {
+        perror("Error while opening remote_cmd_channel");
+        err = eqmt_info->remote_cmd_channel_fd;
+        goto REMOTE_FAIL;
+    }
+    eqmt_info->remote_cmd_addr.sin_family = PF_INET;
+    eqmt_info->remote_cmd_addr.sin_port = htons(net_port);
+    if (inet_pton(AF_INET, ifname, &eqmt_info->remote_cmd_addr.sin_addr.s_addr) == 0) {
+        fprintf(stderr, "Invalid IP adress\n");
+        goto REMOTE_FAIL;
+    }
+    while (retry_cnt < 5) {
+        err = connect(eqmt_info->remote_cmd_channel_fd, (struct sockaddr *)&eqmt_info->remote_cmd_addr,
+                      sizeof(eqmt_info->remote_cmd_addr));
+        if (err == 0)
+            break;
+        if (err < 0) {
+            sleep(3);
+            retry_cnt++;
+            fprintf(stderr, "create_remote_cmd_channel: Connection error retry...%d\n", retry_cnt);
+        }
+    }
+    if (err < 0) {
+        goto REMOTE_FAIL;
+    fprintf(stderr, "create_remote_cmd_channel: Connection %s:%d\n", ifname, net_port);
+    return EXIT_SUCCESS;
+REMOTE_FAIL:
+    fprintf(stderr, "create_remote_cmd_channel: Connection error !!\n");
+    return EXIT_FAILURE;
+}
+
+int simgen_create_udp_cmd_channel(struct simgen_eqmt_info_t *eqmt_info, char *ifname, int net_port) {
+    int err;
+    int retry_cnt = 0;
+    if ((eqmt_info->udp_cmd_channel_fd = socket(AF_INET , SOCK_DGRAM , 0)) < 0) {
+        perror("Error while opening remote_cmd_channel");
+        err = eqmt_info->udp_cmd_channel_fd;
+        goto REMOTE_FAIL;
+    }
+    eqmt_info->udp_cmd_addr.sin_family = AF_INET;
+    eqmt_info->udp_cmd_addr.sin_port = htons(net_port);
+    if (inet_pton(AF_INET, ifname, &eqmt_info->udp_cmd_addr.sin_addr.s_addr) == 0) {
+        fprintf(stderr, "Invalid IP adress\n");
+        goto REMOTE_FAIL;
+    }
+    while (retry_cnt < 5) {
+        err = connect(eqmt_info->udp_cmd_channel_fd, (struct sockaddr *)&eqmt_info->udp_cmd_addr,
+                      sizeof(eqmt_info->udp_cmd_addr));
+        if (err == 0)
+            break;
+        if (err < 0) {
+            sleep(3);
+            retry_cnt++;
+            fprintf(stderr, "create_remote_cmd_channel: Connection error retry...%d\n", retry_cnt);
+        }
+    }
+    if (err < 0) {
+        goto REMOTE_FAIL;
+    fprintf(stderr, "create_remote_cmd_channel: Connection %s:%d\n", ifname, net_port);
+    return EXIT_SUCCESS;
+REMOTE_FAIL:
+    fprintf(stderr, "create_remote_cmd_channel: Connection error !!\n");
+    return EXIT_FAILURE;
+}
+
+
+int remote_cmd_recv(struct simgen_eqmt_info_t *eqmt_info, uint8_t *rx_buff, uint32_t buff_size) {
+    uint32_t offset = 0;
+    int32_t rdlen = 0;
+    struct ethernet_device_info_t *dev_info = priv_data;
+    do {
+        rdlen = recv(eqmt_info->remote_cmd_channel_fd, rx_buff + offset, buff_size - offset, 0);
+        offset += rdlen;
+    } while (offset < buff_size && rdlen > 0);
+    return offset;
+}
+
+int remote_cmd_send(struct simgen_eqmt_info_t *eqmt_info, uint8_t *payload, uint32_t frame_len) {
+    uint32_t offset = 0;
+    int32_t wdlen = 0;
+    while (offset < frame_len) {
+        wdlen = send(eqmt_info->remote_cmd_channel_fd, payload + offset, frame_len - offset, 0);
+        if (wdlen < 0) {
+            //  fprintf(stderr, "[%s:%d] send error: %d\n", __FUNCTION__, __LINE__, wdlen);
+            break;
+        }
+        offset += wdlen;
+    }
+    return offset;
+}
+
+int udp_cmd_sendto(struct simgen_eqmt_info_t *eqmt_info, uint8_t *payload, uint32_t frame_len) {
+    uint32_t offset = 0;
+    int32_t wdlen = 0;
+    while (offset < frame_len) {
+        wdlen = sendto(eqmt_info->udp_cmd_channel_fd, payload + offset, frame_len - offset,
+                       0, (struct sockaddr *)&eqmt_info->udp_cmd_addr, sizeof(eqmt_info->udp_cmd_addr));
+        if (wdlen < 0) {
+            //  fprintf(stderr, "[%s:%d] send error: %d\n", __FUNCTION__, __LINE__, wdlen);
+            break;
+        }
+        offset += wdlen;
+    }
+    return offset;
+}
+
+
 
 int simgen_init(void) {
     int ret;
