2296cad274dc66f22c021468fa310e2c205ffac3
Dung-Ru <tsaidungru.ncku@gmail.com>
2018-04-01 23:04:44 +0800

    driver: use the clean script file path

--- models/device_driver_3rd_party/adv_gpio_pci1737u/clean_driver.sh
+++ models/device_driver_3rd_party/adv_gpio_pci1737u/clean_driver.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-SCRIPT_PATH=$PWD
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DRVER_SOURCE=linux_driver_source_3.2.11.0_64bit
 PRODUCT=pci1737_pci1739
-
+#PRODUCT=pci1730_pcm3730i
 # Build kernel module
 cd  $SCRIPT_PATH/$DRVER_SOURCE/drivers/driver_base/src/lnx_ko
 make clean
