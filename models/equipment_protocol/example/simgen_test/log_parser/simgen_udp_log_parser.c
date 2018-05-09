7c469b9fc844dd404795ed2f517f980c1fc3bef8
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-05-09 17:24:03 +0800

    simgen: Use user's input file name

--- models/equipment_protocol/example/simgen_test/log_parser/simgen_udp_log_parser.c
+++ models/equipment_protocol/example/simgen_test/log_parser/simgen_udp_log_parser.c
@@ -2,20 +2,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include "simgen_udp_cmd.h"
-#define CYCLE_TIME  10 //  ms
 
 int main(int argc, char const *argv[]) {
     /* parse UDP binary log file */
     FILE *input_binary, *output_text;
     struct simgen_udp_command_t udp_cmd;
     int iter;
-    double tx_time;
-
-    if ((input_binary = fopen("UDP_mot.bin", "rb")) == NULL) {
-
+    char file_name[1024] = {};
+    strcpy(file_name, argv[1]);
+    if ((input_binary = fopen(file_name, "rb")) == NULL) {
+        fprintf(stderr, "[ERROR] The file name does not exist\n");
         return EXIT_FAILURE;
     }
-    if ((output_text = fopen("UDP_mot.csv", "w+")) == NULL) {
+    if ((output_text = fopen("UDP_mot.csv", "w")) == NULL) {
         fclose(input_binary);
         return EXIT_FAILURE;
     }
@@ -23,13 +22,12 @@ int main(int argc, char const *argv[]) {
         if (sizeof(udp_cmd) != fread(&udp_cmd, 1, sizeof(udp_cmd), input_binary))
             break ;
 
-        if (udp_cmd.time_of_validity_ms_ == 0) {
-            iter = 0 ;
-            fprintf(output_text,"Iteration, Tx_time_ms, sim_time_ms, latency\n");
+        if (iter == 0) {
+            fprintf(output_text,"Iteration, sim_time_ms, latency\n");
+            continue;
         }
-        tx_time = iter * CYCLE_TIME;
 
-        fprintf(output_text,"%4d, %10.3f, %6d, %d\n", iter, tx_time, udp_cmd.time_of_validity_ms_,
+        fprintf(output_text,"%4d, %6d, %d\n", iter - 1, udp_cmd.time_of_validity_ms_,
                 udp_cmd.latency_wrt_tov_and_current_tir_ms_);
     }
     fclose(input_binary);
