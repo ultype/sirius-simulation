ff2112f99c17e754d0f4067a18cb3ffcf4326a86
Dung-Ru <tsaidungru.ncku@gmail.com>
2018-05-10 20:59:16 +0800

    Default the simgen simulation cycle to 250Hz

--- exe/HIL/cannon_sample_code/RUN_test/input.cpp
+++ exe/HIL/cannon_sample_code/RUN_test/input.cpp
@@ -9,8 +9,8 @@
 extern "C" int run_me() {
     external_clock_switch(&dyn.ext_clk);
     realtime();
-    dyn.simgen_time_jump_per_cycle = 1;
-    dyn.simgen_dev.udp_motion_enable = 0;
+    dyn.simgen_time_jump_per_cycle = 4;
+    dyn.simgen_dev.udp_motion_enable = 1;
     exec_set_terminate_time(200.0);
     return 0;
 }
