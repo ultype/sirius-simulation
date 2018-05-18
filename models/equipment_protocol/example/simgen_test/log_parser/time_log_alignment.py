09ac8b2e13c9f16a103e8ed8493b4188dbbc2ad1
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-05-18 15:46:09 +0800

    parser: rename the parser name to avoid mis understood

--- models/equipment_protocol/example/simgen_test/log_parser/time_log_alignment.py
+++ models/equipment_protocol/example/simgen_test/log_parser/time_log_alignment.py
@@ -31,4 +31,4 @@ for gpsr_elem in enumerate(gpsr_s_nav_arr):
 input_dm_nspo_fileobj.close()
 input_gpsr_s_nav_fileobj.close()
 print "refine_log_nspo.csv\n"
-print "Next step ./error_result_of_simgen.py"
+print "Next step ./combine_result_dm_and_gpsr_log.py"
