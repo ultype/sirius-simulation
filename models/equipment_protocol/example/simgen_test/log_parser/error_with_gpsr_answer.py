e58e4d28b421b92c300e5da08acf691cd947241c
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-05-21 11:38:33 +0800

    Add the root mean square function

--- models/equipment_protocol/example/simgen_test/log_parser/error_with_gpsr_answer.py
+++ models/equipment_protocol/example/simgen_test/log_parser/error_with_gpsr_answer.py
@@ -4,6 +4,9 @@ import itertools
 import sys
 import re
 import math
+
+def get_root_mean_square( mean_square, number):
+    return math.sqrt(mean_square / number)
 # The answer_xxxx.csv is the result of gpsr_s_nav with simgen_log
 csv_answer_fileobj = open('answer_combine_result_dm_and_gpsr_log.csv', 'rbU')
 csv_target_fileobj = open('combine_result_dm_and_gpsr_log.csv', 'rbU')
@@ -25,12 +28,16 @@ csvCursor.writerow(csvHeader)
 
 sim_data_list = []
 iter_idx = 0
+start_flight_idx = 0
+mean_speed_square = 0.0
+mean_length_square = 0.0
 for answer_row, target_row in itertools.izip(answer_arr, target_arr):
     iter_idx += 1
     sim_data_list = []
     if iter_idx <= 1:
         continue
-
+    if float(target_row[0]) == 300.0:
+        start_flight_idx = iter_idx
     for x in xrange(0,8):
         sim_data_list.append(target_row[x])
 # DM Length
@@ -40,15 +47,25 @@ for answer_row, target_row in itertools.izip(answer_arr, target_arr):
 # GPSR ERROR for each axis: answer_gpsr_s_nav - target_gpsr_s_nav
     for x in xrange(0,6):    
         sim_data_list.append(float(answer_row[x+2]) - float(target_row[x+9]))
-# GPSR Length ERROR: answer_gpsr_s_nav - target_gpsr_s_nav
-    sim_data_list.append((float(answer_row[15]) - float(target_row[15])) * 100)
-# GPSR SPEED ERROR: answer_gpsr_s_nav - target_gpsr_s_nav
-    sim_data_list.append((float(answer_row[16]) - float(target_row[16])) * 100)
+    if start_flight_idx >= 300:
+    # GPSR Length ERROR: answer_gpsr_s_nav - target_gpsr_s_nav
+        gpsr_length_err = float(answer_row[15]) - float(target_row[15])
+        sim_data_list.append(gpsr_length_err)
+        mean_length_square = mean_length_square + (gpsr_length_err)**2
+    # GPSR SPEED ERROR cm/s: answer_gpsr_s_nav - target_gpsr_s_nav
+        gpsr_speed_err = float(answer_row[16]) - float(target_row[16])
+        sim_data_list.append(gpsr_speed_err * 100)
+        mean_speed_square = mean_speed_square + (gpsr_speed_err)**2
+        csvCursor.writerow(sim_data_list)
 
-    csvCursor.writerow(sim_data_list)
+
+rms_length = get_root_mean_square(mean_length_square, iter_idx - start_flight_idx)
+rms_speed = get_root_mean_square(mean_speed_square, iter_idx - start_flight_idx) * 100
+
 csv_answer_fileobj.close()
 csv_target_fileobj.close()
 error_file.close()
 
-
-print "error_with_gpsr_snav_answer.csv"
+print ("error_with_gpsr_snav_answer.csv")
+print ( "The position of root mean square is %f meters" % (rms_length))
+print ( "The speed of root mean square is %f cm/s" % (rms_speed))
