b8904f07fcc3a6541ce1021b0f5518d19938fc6f
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-05-28 14:37:57 +0800

    CAN CMD: Change the dsp can command macro

--- models/equipment_protocol/src/flight_computer_agent.c
+++ models/equipment_protocol/src/flight_computer_agent.c
@@ -5,20 +5,20 @@ int fc_can_cmd_dispatch(void *rxframe) {
     struct can_frame *pframe = NULL;
     pframe = (struct can_frame *)rxframe;
     switch (pframe->can_id) {
-        case FC2TVC_III_NO1:
-        case FC2TVC_III_NO2:
-        case FC2TVC_II_NO1:
-        case FC2TVC_II_NO2:
+        case FC_to_TVC_III_NO1:
+        case FC_to_TVC_III_NO2:
+        case FC_to_TVC_II_NO1:
+        case FC_to_TVC_II_NO2:
             qidx = EGSE_TVC_SW_QIDX;
             break;
-        case FC2VALVE_III_NO1:
-        case FC2RCS_III:
-        case FC2ORDNANCE_FAIRING_III:
-        case FC2VALVE_II_NO1:
-        case FC2ORDNANCE_SEPARATION_II:
+        case FC_to_PFS_III:
+        case FC_to_RCS_III:
+        case FC_to_ORDNANCE_FAIRING_III:
+        case FC_to_PFS_II:
+        case FC_to_ORDNANCE_SEPARATION_II:
             qidx = EGSE_EMPTY_SW_QIDX;
             break;
-        case 0x555:
+        case FC_to_EGSE_MISSION_EVENT_FAKE:
             qidx = EGSE_RX_MISSION_EVENT_QIDX;
             break;
         default:
