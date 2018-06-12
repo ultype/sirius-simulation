231c100ae27b17fcefc998d9e111ce4da5c9d36d
Dung-Ru Tsai <dungrutsai@tispace.com>
2018-06-12 15:22:09 +0800

    Add the sdt sample 1pps

--- exe/HIL/sdt_sample_code/RUN_test/input.cpp
+++ exe/HIL/sdt_sample_code/RUN_test/input.cpp
@@ -7,7 +7,7 @@
 #include "../../../xil_common/include/realtime.h"
 
 extern "C" int run_me() {
-    //  external_clock_switch(&dyn.ext_clk);
+    external_clock_switch(&dyn.ext_clk);
     realtime();
     exec_set_terminate_time(300.0);
     return 0;
